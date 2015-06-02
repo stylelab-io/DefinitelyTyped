e04faf51fdf49e9193dba70d027aa98486da8c0c
Tim Jonischkat <tim@emmaeins-media.de>
2015-06-02 19:53:52 +0200

    adding popsicle typing

---
+++ popsicle/popsicle.d.ts
@@ -0,0 +1,47 @@
+/// <reference path="./../q/Q.d.ts" />
+
+declare module "popsicle" {
+
+    interface Request {
+
+        method?: string;
+
+        url: string;
+
+        body?: any;
+
+        headers?: any;
+
+        query?: string;
+
+        timeout?: number;
+
+        parse?: boolean;
+
+        maxRedirects?: number;
+
+        agent?: any;
+
+        jar?: any;
+
+        rejectUnauthorized?: boolean;
+
+        stream?: boolean;
+
+        raw?: boolean;
+
+        encoding?: string;
+
+    }
+
+    interface Response {
+
+        body : string;
+
+    }
+
+    function popsicle(url : string) : Q.IPromise<Response>;
+
+    function popsicle(request : Request) : Q.IPromise<Response>;
+
+}
