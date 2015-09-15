4b07ebe8f5183937ced62b6761d1db955a152243
Christian Jantz <christian.jantz@stylelounge.de>
2015-09-15 14:43:17 +0200

    first version of node console framework typings

---
+++ node-console-framework/node-console-framework.d.ts
@@ -0,0 +1,105 @@
+/// <reference path="../q/Q.d.ts"/>
+/// <reference path="../typeioc/typeioc.d.ts"/>
+/// <reference path="../node/node.d.ts"/>
+
+declare module 'node-console-framework' {
+
+    export interface IArgument {
+        name: string;
+        required: boolean;
+        type: any;
+        alias: string;
+        description: string;
+        defaultValue ? : any;
+        validation?: any;
+    }
+
+    export interface ICommand {
+        getName(): string;
+        getDescription(): string;
+        getArguments(): IArgument[];
+        factory(): Typeioc.IFactory<ICommand>;
+        execute(): Q.Promise<any>;
+    }
+
+    export class Container {
+        constructor();
+        get<T>(name: string): any;
+        registerService(name:string, creator: Typeioc.IFactory<any>): Container;
+    }
+
+    export class ArgvInput {
+        constructor(serviceBuilder: Typeioc.IContainer, argv: any);
+        initCommands(): ArgvInput;
+        addCommand(cmd: ICommand): ArgvInput;
+    }
+
+    export interface IInput {
+        getCommand(): string;
+        getArguments(): string[];
+        getArgument(index: number): string;
+        getOptions(): any;
+        getOption(key: string): IOption;
+        hasOption(key: string): boolean;
+    }
+
+    export class Input {
+        command: string;
+        arguments: string[];
+        options: any;
+
+        getCommand(): string;
+        getArguments(): string[];
+        getArgument(index: number): string;
+        getOptions(): any;
+        getOption(key: string): IOption;
+        hasOption(key: string): boolean;
+    }
+
+    export interface IOption {
+        getKey(): string;
+        getValue(): string;
+    }
+
+    export class ConsoleOutput extends Output {
+        stdout: NodeJS.WritableStream;
+        stderr: NodeJS.WritableStream;
+
+        constructor(stdout: NodeJS.WritableStream, stderr: NodeJS.WritableStream);
+        write(string: string);
+        writeErr(string: string);
+        format(...args: any[]);
+    }
+
+    export interface IOutput {
+        write(string: string);
+        writeErr(string: string);
+        format(...args:  any[]);
+    }
+
+    export abstract class Output {
+        write(string: string);
+        writeErr(string: string);
+        format(...args:  any[]);
+    }
+
+    export interface IService {
+        getName(): string;
+        factory(sc: Typeioc.IContainer): Typeioc.IFactory<IService>;
+    }
+
+    export class ApplicationKernel {
+        registeredCommands: ICommand[];
+        registeredServices: IService[];
+
+        constructor();
+        preRunHook(app: ConsoleApplication);
+        postRunHook(app: ConsoleApplication);
+    }
+
+    export class ConsoleApplication {
+        constructor(kernel: ApplicationKernel);
+        constructor(kernel: ApplicationKernel, env: string);
+        run();
+    }
+}
